d6cd4e570fc4c9e049417960c8c3dcc54981efeb
Sonny Mayta <mayta.sonny@gmail.com>
2024-02-22 14:52:13 -0400

    Configuración y depurado

---
+++ Variables/dist/variables.js
@@ -0,0 +1,6 @@
+"use strict";
+let saludo = "Hola, mundo!";
+console.log(saludo);
+saludo = "Nos vemos";
+console.log(saludo);
+//# sourceMappingURL=variables.js.map
---
+++ Variables/src/variables.ts
@@ -0,0 +1,4 @@
+let saludo: string = "Hola, mundo!";
+console.log(saludo);
+saludo = "Nos vemos";
+console.log(saludo);
