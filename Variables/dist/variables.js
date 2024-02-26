a0fdfc298f0f50895271b8de67ada2ebdde41c83
Sonny Mayta <mayta.sonny@gmail.com>
2024-02-26 10:12:06 -0400

    Variables

--- Variables/dist/variables.js
+++ Variables/dist/variables.js
@@ -1,6 +1,11 @@
 "use strict";
-let saludo = "Hola, mundo!";
-console.log(saludo);
-saludo = "Nos vemos";
-console.log(saludo);
+let valorNumerico = 3000;
+let caracterAlfabetico = "palabra";
+let estadoActual = false;
+console.log(typeof (valorNumerico));
+console.log(typeof (caracterAlfabetico));
+console.log(typeof (estadoActual));
+let numero = 200;
+let nombre = "Carlos";
+let estado = true;
 //# sourceMappingURL=variables.js.map
--- Variables/src/variables.ts
+++ Variables/src/variables.ts
@@ -1,4 +1,14 @@
-let saludo: string = "Hola, mundo!";
-console.log(saludo);
-saludo = "Nos vemos";
-console.log(saludo);
+// Variables
+let valorNumerico = 3000;
+let caracterAlfabetico = "palabra";
+let estadoActual = false;
+
+// Si inicializamos una variable en ts a este se le asignara el tipo de dato
+console.log(typeof(valorNumerico));
+console.log(typeof(caracterAlfabetico));
+console.log(typeof(estadoActual));
+
+// En cambio es recomendable inicializar las variables con sus tipos correspondientes
+let numero: number = 200;
+let nombre: string = "Carlos";
+let estado: boolean = true;
